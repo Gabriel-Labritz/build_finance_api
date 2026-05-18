a832dd6ed6e447419c4784f006a51f05684eb4db
Gabriel Labritz <gabriellabritz@gmail.com>
2026-05-18 12:03:13 -0300

    Primeiro commit: Projeto com as configurações iniciais

---
+++ src/main/java/com/gabriellabritz/build_finance_api/BuildFinanceApiApplication.java
@@ -0,0 +1,11 @@
+package com.gabriellabritz.build_finance_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuildFinanceApiApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(BuildFinanceApiApplication.class, args);
+	}
+}
---
+++ src/test/java/com/gabriellabritz/build_finance_api/BuildFinanceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabriellabritz.build_finance_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuildFinanceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
