a336355195bec354f503f66652342ecf913fd231
Gabriel Labritz <gabriellabritz@gmail.com>
2026-05-20 15:46:26 -0300

    test: Adiciona testes unitários para verifyAccount AuthService

--- src/test/java/com/gabriellabritz/build_finance_api/domain/auth/AuthServiceTest.java
+++ src/test/java/com/gabriellabritz/build_finance_api/domain/auth/AuthServiceTest.java
@@ -1,10 +1,14 @@
 package com.gabriellabritz.build_finance_api.domain.auth;
 
 import com.gabriellabritz.build_finance_api.domain.auth.dtos.requests.AuthRegisterRequestDto;
+import com.gabriellabritz.build_finance_api.domain.auth.verification.EmailVerificationToken;
 import com.gabriellabritz.build_finance_api.domain.auth.verification.EmailVerificationTokenRepository;
+import com.gabriellabritz.build_finance_api.domain.user.User;
 import com.gabriellabritz.build_finance_api.domain.user.UserRepository;
 import com.gabriellabritz.build_finance_api.infra.email.EmailService;
 import com.gabriellabritz.build_finance_api.infra.exceptions.auth.EmailAlreadyUsedException;
+import com.gabriellabritz.build_finance_api.infra.exceptions.auth.InvalidVerificationTokenException;
+import com.gabriellabritz.build_finance_api.infra.exceptions.auth.UserAlreadyVerifiedException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
@@ -14,6 +18,8 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
@@ -36,11 +42,17 @@ class AuthServiceTest {
     @Mock
     private PasswordEncoder passwordEncoder;
 
+    private EmailVerificationToken emailVerificationToken;
+
+    private User user;
+
     private AuthRegisterRequestDto authRegisterRequestDto;
 
     @BeforeEach
     void setUp() {
         this.authRegisterRequestDto = new AuthRegisterRequestDto("Teste", "teste@email.com", "Senha@123");
+        this.emailVerificationToken = mock(EmailVerificationToken.class);
+        this.user = mock(User.class);
     }
 
     @Nested
@@ -97,7 +109,7 @@ class AuthServiceTest {
                             token.getUser() != null &&
                                     token.getToken() != null &&
                                     !token.getToken().isBlank() &&
-                                    token.isExpired()
+                                    !token.isExpired()
                     ));
         }
 
@@ -116,6 +128,63 @@ class AuthServiceTest {
             // Assert
             verify(emailService).sendEmailVerification(argThat(user ->
                     user.getEmail().equals(authRegisterRequestDto.email())), any());
+        }
+    }
+
+    @Nested
+    class verifyAccount {
+        @Test
+        @DisplayName("Deve lançar a exceção InvalidVerificationTokenException para um token inválido")
+        void shouldThrowInvalidVerificationTokenExceptionWhenTokenIsInvalid() {
+            // Arrange
+            when(emailVerificationTokenRepository.findByToken("token-invalid")).thenReturn(Optional.empty());
+
+            // Act + Assert
+            assertThrows(InvalidVerificationTokenException.class, () -> authService.verifyAccount("token-invalid"));
+            verify(emailVerificationTokenRepository, never()).delete(any());
+        }
+
+        @Test
+        @DisplayName("Deve lançar a exceção InvalidVerificationTokenException para um token expirado")
+        void shouldThrowInvalidVerificationTokenExceptionWhenTokenIsExpired() {
+            // Arrange
+            when(emailVerificationTokenRepository.findByToken("token-expired")).thenReturn(Optional.of(emailVerificationToken));
+            doThrow(new InvalidVerificationTokenException("Token de verificação expirado."))
+                    .when(emailVerificationToken).validate();
+
+            // Act + Assert
+            assertThrows(InvalidVerificationTokenException.class, () -> authService.verifyAccount("token-expired"));
+            verify(emailVerificationTokenRepository, never()).delete(any());
+        }
+
+        @Test
+        @DisplayName("Deve lançar a exceção UserAlreadyVerifiedException para um usuário já verificado")
+        void shouldThrowUserAlreadyVerifiedExceptionWhenUserAlreadyVerified() {
+            // Arrange
+            when(emailVerificationTokenRepository.findByToken("token-valid")).thenReturn(Optional.of(emailVerificationToken));
+            when(emailVerificationToken.getUser()).thenReturn(user);
+            doThrow(new UserAlreadyVerifiedException("O usuário já foi verificado."))
+                    .when(user).verify();
+
+            // Act + Assert
+            assertThrows(UserAlreadyVerifiedException.class, () -> authService.verifyAccount("token-valid"));
+            verify(emailVerificationTokenRepository, never()).delete(any());
+        }
+
+        @Test
+        @DisplayName("Deve verificar o usuário e deletar o token quando o token é válido.")
+        void shouldVerifyUserAndDeleteTokenWhenValid() {
+            // Arrange
+            when(emailVerificationTokenRepository.findByToken("token-valid")).thenReturn(Optional.of(emailVerificationToken));
+            when(emailVerificationToken.getUser()).thenReturn(user);
+
+            // Act
+            authService.verifyAccount("token-valid");
+
+            // Assert
+            verify(emailVerificationToken).validate();
+            verify(user).verify();
+            verify(emailVerificationTokenRepository).delete(emailVerificationToken);
         }
     }
 }
